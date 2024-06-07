576b622b29f47e7aef1118dfdb476e8055d5c28a
NetflixClone <hosiduc2002.chem@gmail.com>
2024-06-07 13:24:12 +0700

    feat:Adding app sidebar

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,7 +1,11 @@
+import { List } from "./list";
+import { NewButton } from "./new-button";
+
 const SideBar = () => {
   return (
-    <div className="fixed left-0 z-[1] h-full w-[60px] flex-col gap-y-4 bg-red-300">
-      Side
+    <div className="fixed left-0 z-[1] flex h-full w-[60px] flex-col gap-y-4 bg-slate-800 p-3 text-white">
+      <List />
+      <NewButton />
     </div>
   );
 };
---
+++ app/(dashboard)/_components/sidebar/list.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useOrganizationList } from "@clerk/nextjs";
+import { Item } from "./item";
+
+export const List = () => {
+  const { userMemberships } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+
+  if (!userMemberships.data?.length) return null;
+
+  return (
+    <ul className="space-y-4">
+      {userMemberships.data?.map((men) => (
+        <Item
+          key={men.organization.id}
+          id={men.organization.id}
+          name={men.organization.name}
+          imageUrl={men.organization.imageUrl}
+        />
+      ))}
+    </ul>
+  );
+};
