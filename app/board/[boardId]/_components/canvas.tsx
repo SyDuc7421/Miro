1309317bafbccf47754c9fb59eb3e8c453d3c4e1
NetflixClone <hosiduc2002.chem@gmail.com>
2024-06-14 11:04:40 +0700

    feat: Add Canvas type and current Cancas state

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,18 +1,38 @@
 "use client";
 
+import { useState } from "react";
+
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
+
+import { CanvasState, CanvasMode } from "@/types/canvas";
+import { useCanRedo, useCanUndo, useHistory } from "@/liveblocks.config";
 
 interface CanvasProps {
   boardId: string;
 }
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+
   return (
     <main className="relative h-full w-full touch-none ">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        redu={history.redo}
+        undo={history.undo}
+        canRedo={canRedo}
+        canUndo={canUndo}
+      />
     </main>
   );
 };
